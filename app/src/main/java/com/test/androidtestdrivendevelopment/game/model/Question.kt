2b0d2e99e71129c20c1affcecaa72a10f3bf24bb
Prateek Bhambhu <prateek.dev121@gmail.com>
2025-01-11 16:40:49 +0530

    - update question test

--- app/src/main/java/com/test/androidtestdrivendevelopment/game/model/Question.kt
+++ app/src/main/java/com/test/androidtestdrivendevelopment/game/model/Question.kt
@@ -1,9 +1,25 @@
 package com.test.androidtestdrivendevelopment.game.model
 
 class Question(
-    val correctOption:String,
-    val incorrectOption:String,
+    val correctOption: String,
+    val incorrectOption: String,
 ) {
-    var answeredOption:String? = null
+    var answeredOption: String? = null
         private set
+
+    val isAnsweredCorrectly: Boolean
+        get() = answeredOption == correctOption
+
+
+    fun answer(value: String): Boolean {
+
+        if(value != correctOption && value != incorrectOption){
+            throw IllegalArgumentException("Not a valid answer")
+        }
+
+        answeredOption = value
+
+        return isAnsweredCorrectly
+
+    }
 }
--- app/src/test/java/com/test/androidtestdrivendevelopment/game/model/QuestionUnitTest.kt
+++ app/src/test/java/com/test/androidtestdrivendevelopment/game/model/QuestionUnitTest.kt
@@ -1,15 +1,46 @@
 package com.test.androidtestdrivendevelopment.game.model
 
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
+import java.lang.IllegalArgumentException
 
 class QuestionUnitTest {
 
+    // Declares a private lateinit variable for the Question object, to be initialized before each test
+    private lateinit var question: Question
+
+    @Before
+    // The method annotated with @Before is executed before each test method.
+    // It initializes the Question object to ensure every test starts with a fresh instance.
+    fun initiateQuestion() {
+        question = Question(correctOption = "CORRECT", incorrectOption = "INCORRECT")
+    }
+
     @Test
-    fun whenCreatingQuestion_answeredOptionShouldBeNull(){
-
-        val question = Question(correctOption = "CORRECT", incorrectOption = "INCORRECT")
-
+    // Indicates that this is a test method. It verifies the initial state of the 'answeredOption' property in the Question object.
+    fun whenCreatingQuestion_answeredOptionShouldBeNull() {
         Assert.assertNull(question.answeredOption)
+    }
+
+    @Test
+    // Marks this as a test method. It checks that calling 'answer()' with the correct option returns true.
+    fun whenAnswering_withCorrectOption_shouldReturnTrue() {
+        val result = question.answer("CORRECT")
+        Assert.assertEquals(true, result)
+    }
+
+    @Test
+    // Denotes a test method. This test ensures that providing an incorrect answer to 'answer()' returns false.
+    fun whenAnswering_whenInCorrectAnswer_shouldReturnFalse() {
+        val result = question.answer("INCORRECT")
+        Assert.assertEquals(false, result)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    // Specifies that this test method expects an IllegalArgumentException to be thrown.
+    // It tests that an invalid option passed to 'answer()' results in an exception.
+    fun whenAnswering_whenInvalidOption_throwException() {
+        question.answer(value = "INVALID")
     }
 }
